a696951f86a084e54d25800736a558bd1d6c9dc2
RajuA3112 <48004458+RajuA3112@users.noreply.github.com>
2023-08-03 23:18:53 +0530

    Add files via upload

---
+++ Block/Index/Questions.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Codilar\ProductOrderSuit\Block\Index;
+
+class Questions extends \Magento\Framework\View\Element\Template
+{
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\View\Element\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+}
+
